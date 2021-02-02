1b2485798465be59fe82cfc36bd11473bccc0ea9
ybybwdwd <957384716@qq.com>
2021-02-02 22:59:22 +0800

    udp_epoll_chat

--- client.c
+++ client.c
@@ -2,67 +2,99 @@
 
 #define MAXFD 10
 
+int serverFd;
+struct sockaddr_in serAddr;
+socklen_t len;
+
 typedef struct
 {
-    char name[64];
+    char ip[64];
+    uint16_t port;
     char msg[128];
+    short link;
 }Messege;
+
+void sigFunc(int signum)
+{
+    Messege msg;
+    memset(&msg,0,sizeof(msg));
+
+    sendto(serverFd,&msg,sizeof(msg),0,(struct sockaddr*)&serAddr,len);
+}
 
 int main(int argc,char *argv[])
 {
     ARGS_CHECK(argc,3);
 
     //创建socket            ipv4    tcp
-    int serverFd=socket(AF_INET,SOCK_STREAM,0);
+    serverFd=socket(AF_INET,SOCK_DGRAM,0);
     ERROR_CHECK(serverFd,-1,"socket");
 
     //记录ip和端口
-    struct sockaddr_in serAddr;
     memset(&serAddr,0,sizeof(serAddr));
     serAddr.sin_family=AF_INET;//ipv4
     serAddr.sin_addr.s_addr=inet_addr(argv[1]);//ip格式转化
     serAddr.sin_port=htons(atoi(argv[2]));//设置端口为网络字节序
     
-    //connect连接服务器
-    int ret=connect(serverFd,(struct sockaddr*)&serAddr,
-                    sizeof(serAddr));
-    ERROR_CHECK(ret,-1,"connect");
+    len=sizeof(serAddr);
 
     Messege msg;
     memset(&msg,0,sizeof(msg));
 
-    fd_set rdset;
-    FD_ZERO(&rdset);
+    msg.link=1;
+    int ret=sendto(serverFd,&msg,sizeof(msg),0,(struct sockaddr*)&serAddr,len);
+    ERROR_CHECK(ret,-1,"sendto");
 
-    //select的返回值是可读文件描述符的数量
+    //创建一个epoll实例，参数为非0正数，无含义
+    int epfd=epoll_create(1);
+    ERROR_CHECK(epfd,-1,"epoll_create");
+
+    //把描述符交给epoll管理
+    struct epoll_event evt,cliEvt[3];
+    memset(&evt,0,sizeof(evt));
+
+    //添加serverFD
+    evt.events=EPOLLIN|EPOLLET;
+    evt.data.fd=serverFd;
+    ret=epoll_ctl(epfd,EPOLL_CTL_ADD,serverFd,&evt);
+    ERROR_CHECK(ret,-1,"epoll_ctl");
+
+    //添加标准输入
+    evt.events = EPOLLIN;
+    evt.data.fd = STDIN_FILENO;
+    ret = epoll_ctl(epfd,EPOLL_CTL_ADD,STDIN_FILENO,&evt);
+    ERROR_CHECK(ret,-1,"epoll_ctl");
     int readyNum =0;
+
+    signal(SIGINT,sigFunc);
 
     while(1)
     {
-        FD_SET(STDIN_FILENO,&rdset); 
-        FD_SET(serverFd,&rdset); 
-        readyNum = select(MAXFD,&rdset,NULL,NULL,NULL);
+        readyNum=epoll_wait(epfd,cliEvt,3,-1);
+        ERROR_CHECK(readyNum,-1,"epoll_wait");
 
         if(readyNum>0)
         {
-            //向服务器发送数据
-            if(FD_ISSET(STDIN_FILENO,&rdset))
+            for(int i=0;i<readyNum;i++)
             {
-                memset(&msg,0,sizeof(msg));
-                read(STDIN_FILENO,msg.msg,sizeof(msg.msg));
-                send(serverFd,&msg,sizeof(msg),0);
-            }
-
-            //接收服务器的数据
-            if(FD_ISSET(serverFd,&rdset)){
-                memset(&msg,0,sizeof(msg));
-                ret = recv(serverFd,&msg,sizeof(msg),0);
-                if(0 == ret)
+                //服务器发消息过来
+                if(cliEvt[i].data.fd==serverFd)
                 {
-                    printf("服务器断开连接\n");
-                    break;
+                    memset(&msg,0,sizeof(msg));
+                    ret=recvfrom(serverFd,&msg,sizeof(msg),0,
+                                 (struct sockaddr*)&serAddr,&len);
+                    ERROR_CHECK(ret,-1,"recvfrom");
+                    printf("%s %d:\n%s",msg.ip,msg.port,msg.msg);
                 }
-                printf("%s:\n%s",msg.name,msg.msg);
+                //发消息给服务器
+                if(cliEvt[i].data.fd==STDIN_FILENO)
+                {
+                    memset(&msg,0,sizeof(msg));
+                    msg.link=1;
+                    read(STDIN_FILENO,msg.msg,sizeof(msg.msg));
+                    int ret=sendto(serverFd,&msg,sizeof(msg),0,(struct sockaddr*)&serAddr,len);
+                    ERROR_CHECK(ret,-1,"sendto");
+                }
             }
         }
     }
--- server.c
+++ server.c
@@ -4,30 +4,48 @@
 
 typedef struct
 {
-    int clientFd;
+    struct sockaddr_in cliAddr;
     int stat;//连接状态，0未连接，1已连接
 }Client;
 
 //发送的消息
 typedef struct
 {
-    char name[64];
+    char ip[64];
+    uint16_t port;
     char msg[128];
+    short link;
 }Messege;
+
+//保存文件描述符当前的状态
+int setNonblock(int fd)
+{
+    int status=0;
+    status = fcntl(fd,F_GETFL);
+    ERROR_CHECK(status,-1,"F_GETFL");
+    status = status|O_NONBLOCK;
+    int ret = fcntl(fd,F_SETFL,status);
+    ERROR_CHECK(ret,-1,"F_SETFL");
+    return 0;
+}
 
 int main(int argc,char *argv[])
 {
     ARGS_CHECK(argc,3);
 
-    //创建socket            ipv4    tcp
-    int listenFd=socket(AF_INET,SOCK_STREAM,0);
-    ERROR_CHECK(listenFd,-1,"socket");
+    //创建socket            ipv4    udp
+    int serverFd=socket(AF_INET,SOCK_DGRAM,0);
+    ERROR_CHECK(serverFd,-1,"socket");
 
     //设置地址可重用，允许绑定TIME_WAIT状态的地址
     int reuse=1;
     int ret=0;
-    ret=setsockopt(listenFd,SOL_SOCKET,SO_REUSEADDR,&reuse,sizeof(reuse));
+    ret=setsockopt(serverFd,SOL_SOCKET,SO_REUSEADDR,&reuse,
+                   sizeof(reuse));
     ERROR_CHECK(ret,-1,"setsockopt");
+
+    //设置非阻塞
+    setNonblock(serverFd);
 
     //记录ip和端口
     struct sockaddr_in serAddr;
@@ -37,85 +55,94 @@ int main(int argc,char *argv[])
     serAddr.sin_port=htons(atoi(argv[2]));//设置端口为网络字节序
     
     //bind绑定ip和端口     把新类型转成老类型
-    ret=bind(listenFd,(struct sockaddr*)&serAddr,
-                 sizeof(serAddr));
+    ret=bind(serverFd,(struct sockaddr*)&serAddr,sizeof(serAddr));
     ERROR_CHECK(ret,-1,"bind");
 
-    //listen监听        指定同时能处理的最大连接要求，最大128
-    ret=listen(listenFd,10);
-    ERROR_CHECK(ret,-1,"listen");
-
+    //客户端ip和端口
     Client client[10];
     memset(&client,0,sizeof(client));
+    socklen_t len[10];
+    for(int i=0;i<10;i++)
+    {
+        len[i]=sizeof(client[i].cliAddr);
+    }
 
     Messege msg;
     memset(&msg,0,sizeof(msg));
 
-    fd_set rdset;
-    FD_ZERO(&rdset);
+    //创建一个epoll实例，参数为非0正数，无含义
+    int epfd=epoll_create(1);
+    ERROR_CHECK(epfd,-1,"epoll_create");
 
-    fd_set moniterSet;
-    FD_ZERO(&moniterSet);
-    FD_SET(listenFd,&moniterSet);
+    //把描述符交给epoll管理
+    struct epoll_event evt,cliEvt[20];
+    memset(&evt,0,sizeof(evt));
 
-    //select返回值是可读文件描述符的数量
+    evt.events=EPOLLIN|EPOLLET;
+    evt.data.fd=serverFd;
+    ret=epoll_ctl(epfd,EPOLL_CTL_ADD,serverFd,&evt);
+    ERROR_CHECK(ret,-1,"epoll_ctl");
+
     int readyNum=0;
+
+    struct sockaddr_in tmpAddr;
+    memset(&tmpAddr,0,sizeof(tmpAddr));
+    socklen_t tmpLen=sizeof(tmpAddr);
 
     while(1)
     {
-        FD_ZERO(&rdset);
-
-        memcpy(&rdset,&moniterSet,sizeof(moniterSet));
-
-        readyNum=select(MAXFD,&rdset,NULL,NULL,NULL);
-        ERROR_CHECK(readyNum,-1,"select");
-
+        readyNum=epoll_wait(epfd,cliEvt,20,-1);
+        ERROR_CHECK(readyNum,-1,"epoll_wait");
         if(readyNum>0)
         {
-            //accept
-            if(FD_ISSET(listenFd,&rdset))
+            for(int i=0;i<readyNum;i++)
             {
-                for(int j=0;j<10;j++)
+                //有新客户端连接，或有消息发过来
+                if(cliEvt[i].data.fd==serverFd)
                 {
-                    if(0==client[j].stat)
-                    {
-                        client[j].clientFd=accept(listenFd,
-                                                  NULL,NULL);
-                        client[j].stat=1;
-                        printf("客户端%d已连接\n",
-                               client[j].clientFd);
-                        FD_SET(client[j].clientFd,&moniterSet);
-                        break;
-                    }
-                }
-            }
-
-            //接收客户端发来的数据，并发送给其他客户端
-            for(int i=0;i<10;i++)
-            {
-                if(FD_ISSET(client[i].clientFd,&rdset)&&1==client[i].stat)
-                {
+                    int j;
                     memset(&msg,0,sizeof(msg));
-                
-                    ret=recv(client[i].clientFd,&msg,
-                             sizeof(msg),0);
-                    if(0==ret)
+                    ret=recvfrom(serverFd,&msg,sizeof(msg),0,
+                                 (struct sockaddr*)&tmpAddr,
+                                 &tmpLen);
+                    ERROR_CHECK(ret,-1,"recvfrom");
+
+                    for(j=0;j<10;j++)
                     {
-                        printf("客户端%d断开连接\n",
-                               client[i].clientFd);
-                        close(client[i].clientFd);
-                        FD_CLR(client[i].clientFd,&moniterSet);
-                        memset(&client[i],0,sizeof(client[i]));
-                    }
-                    else
-                    {
-                        sprintf(msg.name,"客户端%d",
-                                client[i].clientFd);
-                        for(int j=0;j<10;j++)
+                        //ip和端口相同，则发送信息
+                        if(client[j].cliAddr.sin_addr.s_addr==tmpAddr.sin_addr.s_addr&&client[j].cliAddr.sin_port==tmpAddr.sin_port&&1==msg.link)
                         {
-                            if(j!=i&&1==client[j].stat)
-                                send(client[j].clientFd,
-                                     &msg,sizeof(msg),0);
+                            for(int k=0;k<10;k++)
+                            {
+                                if(k==j)
+                                {
+                                    continue;
+                                }
+                                strcpy(msg.ip,inet_ntoa(tmpAddr.sin_addr));
+                                msg.port=tmpAddr.sin_port;
+                                sendto(serverFd,&msg,sizeof(msg),0,(struct sockaddr*)&client[k].cliAddr,len[k]);
+                            }
+                            break;
+                        }
+                        else if(client[j].cliAddr.sin_addr.s_addr==tmpAddr.sin_addr.s_addr&&client[j].cliAddr.sin_port==tmpAddr.sin_port&&0==msg.link)
+                        {
+                            printf("客户端%d退出\n",j);
+                            memset(&client[j],0,sizeof(client[j]));
+                            break;
+                        }
+                    }
+
+                    if(j==10)//无相同ip和端口，新连接
+                    {
+                        for(int k=0;k<10;k++)
+                        {
+                            if(0==client[k].stat)
+                            {
+                                memcpy(&client[k].cliAddr,&tmpAddr,sizeof(tmpAddr));
+                                client[k].stat=1;
+                                printf("客户端%d连接\n",k);
+                                break;
+                            }
                         }
                     }
                 }
@@ -123,7 +150,6 @@ int main(int argc,char *argv[])
         }
     }
     
-    close(listenFd);
     return 0;
 }
 
